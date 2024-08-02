d071f149640feb7142054c628f1183423eaad1ab
Akaimnky <bhairabm1908@gmail.com>
2024-08-03 03:44:34 +0530

    added a possibility of adding multiple youtube channels in the channel_id of config.js

--- config.js
+++ config.js
@@ -4,7 +4,11 @@ module.exports = {
   messageTemplate:
     "Hello everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
   //dont change messageTemplate
-  channel_id: "advanced settings (in pc) -> channel id",
+  channel_id: [
+    "advanced settings (in pc) -> channel id",
+    "channel id2",
+    "channel id3 etc",
+  ],
   watchInterval: 10000,
   youtubeApiKey: "Token from google cloud platform",
 };
--- index.js
+++ index.js
@@ -10,24 +10,27 @@ let lastVideoId = null;
 
 async function checkYouTube() {
   try {
-    const response = await axios.get(
-      `https://www.googleapis.com/youtube/v3/search?key=${config.youtubeApiKey}&channelId=${config.channel_id}&part=snippet,id&order=date&maxResults=1`
-    );
+    for (const channelId of config.channel_id) {
+      const response = await axios.get(
+        `https://www.googleapis.com/youtube/v3/search?key=${config.youtubeApiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=1`
+      );
 
-    const video = response.data.items[0];
+      const video = response.data.items[0];
 
-    console.log("Checking for new video:", video);
-    if (video.id.videoId !== lastVideoId) {
-      lastVideoId = video.id.videoId;
-      const channel = await client.channels.fetch(config.channel);
-      const message = config.messageTemplate
-        .replace("{author}", video.snippet.channelTitle)
-        .replace("{title}", video.snippet.title)
-        .replace(
-          "{url}",
-          `https://www.youtube.com/watch?v=${video.id.videoId}`
-        );
-      channel.send(message);
+      console.log(`Checking for new video on channel ${channelId}:`, video);
+
+      if (video.id.videoId !== lastVideoId) {
+        lastVideoId = video.id.videoId;
+        const channel = await client.channels.fetch(channelId);
+        const message = config.messageTemplate
+          .replace("{author}", video.snippet.channelTitle)
+          .replace("{title}", video.snippet.title)
+          .replace(
+            "{url}",
+            `https://www.youtube.com/watch?v=${video.id.videoId}`
+          );
+        channel.send(message);
+      }
     }
   } catch (error) {
     console.error("Error fetching YouTube data:", error);
